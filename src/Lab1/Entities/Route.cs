d44a0ecbddfd75c8603bd36a137c3e4e192bb88d
Ivan Erofeev <ivanerofeev01@gmail.com>
2025-10-04 16:52:09 +0300

    chore: fixed extra whitespace

--- src/Lab1/Entities/Route.cs
+++ src/Lab1/Entities/Route.cs
@@ -8,7 +8,7 @@ public sealed class Route
 
     public Route(IReadOnlyCollection<IRouteSegment> segments, double endSpeedLimit)
     {
-        ArgumentNullException.ThrowIfNull(segments,  nameof(segments));
+        ArgumentNullException.ThrowIfNull(segments, nameof(segments));
 
         if (segments.Count == 0)
             throw new ArgumentException("Route must have at least one segment", nameof(segments));
