330002ffd794cc5487cf4c0faa0a923678aa3fbd
Ivan Erofeev <ivanerofeev01@gmail.com>
2025-10-04 14:59:30 +0300

    feat: intial commit with main interface

---
+++ src/Lab1/Segments/IRouteSegment.cs
@@ -0,0 +1,8 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab1.Entities;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Segments;
+
+public interface IRouteSegment
+{
+    TraversalContext Traverse(Train train);
+}
